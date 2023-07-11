8f56ba4ce689e4f543114fc283bc1033749880aa
Mailson Maia Alves <59203034+mailsonm@users.noreply.github.com>
2023-07-11 09:32:40 -0300

    personagem adicionado
    mudado a orientação do jogo para horizontal
    
    adicionado o personagem com animação de andar e seu script de movimentação

---
+++ Witchy-Run/Assets/Scripts/PersonagemMovimento.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemMovimento : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
+    private int jumpCount;
+    [SerializeField] private int maxJumpCount = 2; // Define o número máximo de pulos permitidos
+
+    [SerializeField] private float forcaPulo = 550f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (isGrounded)
+        {
+            jumpCount = 0; // Reinicia o contador de pulos se estiver no chão
+            animator.SetBool("IsJumping", false);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0) && jumpCount < maxJumpCount)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * forcaPulo);
+        animator.SetBool("IsJumping", true);
+        jumpCount++;
+    }
+}
