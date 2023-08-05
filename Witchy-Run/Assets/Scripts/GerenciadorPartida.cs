c8d83b412bd3a56638fc75d67159f6715056443a
Mailson Maia Alves <59203034+mailsonm@users.noreply.github.com>
2023-08-05 10:59:36 -0300

    finalização do jogo
    colocando pontuação e e finaliando o jogo, falta corrigir alguns bugs menu e pequenas correções mais de modo geral está finalizado.

--- Witchy-Run/Assets/Scripts/GerenciadorPartida.cs
+++ Witchy-Run/Assets/Scripts/GerenciadorPartida.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GerenciadorPartida : MonoBehaviour
 {
     private bool partidaIniciada;
-    private void Awake() {
+
+    private void Awake() 
+    {
         Time.timeScale = 1;
+        Application.targetFrameRate = 60;
     }
     
     // Update is called once per frame
     void Update()
     {
         if (partidaIniciada) return;
+
         if (Input.GetMouseButtonDown(0))
         {
             partidaIniciada = true;
             Time.timeScale = 1;
         }
+    }
+
+    public void ReiniciarPartida()
+    {
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Witchy-Run/Assets/Scripts/Pontuacao.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pontuacao : MonoBehaviour
+{
+    private float tempo;
+    private int pontuacao;
+
+    [SerializeField] private TMP_Text pontuacaoText;
+    [SerializeField] private TMP_Text pontuacaoGameOverText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        pontuacao = (int)(tempo * 5);
+
+        pontuacaoText.text = pontuacao.ToString("000000");
+        pontuacaoGameOverText.text = pontuacao.ToString("000000");
+        
+    }
+}
